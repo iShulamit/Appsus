800bfa5c4c2e13922b3e8460423d1aa4723ca699
iShulamit <66863006+iShulamit@users.noreply.github.com>
2020-12-24 22:19:09 +0200

    header basic css

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -2,22 +2,24 @@ const { NavLink, withRouter } = ReactRouterDOM;
 
 function _AppHeader(props) {
 
-    console.log('props:', props);
+    // console.log('props:', props);
 
-    function goTo(){
+    function goTo() {
         props.history.push('/emails')
     }
 
     return (
-        <nav>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/email">Email</NavLink></li>
-                <li><NavLink to="/keep">Keep</NavLink></li>
-                <li><NavLink to="/book">Books</NavLink></li>
-                <button onClick={goTo}>goTo</button>
-            </ul>
-        </nav>
+        <header className="app-header">
+            <nav>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/email">Email</NavLink></li>
+                    <li><NavLink to="/keep">Keep</NavLink></li>
+                    <li><NavLink to="/book">Books</NavLink></li>
+                    <button onClick={goTo}>goTo</button>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
