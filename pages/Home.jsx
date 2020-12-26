b6348617bdc079e8064a1c2da4dfa6a724414725
iShulamit <66863006+iShulamit@users.noreply.github.com>
2020-12-26 22:31:48 +0200

    home link imgs route

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -11,9 +11,9 @@ export class Home extends React.Component {
             <section className="home-page">
                 {/* <div className="home-title"><h1>Welcome to my Appsus</h1></div> */}
                 <div className="home-links">
-                    <Link to={"/email"}><img src="../assets/img/email.png" /></Link>
-                    <Link to={"/keep"}><img src="../assets/img/keep.png" /></Link>
-                    <Link to={"/book"}><img src="../assets/img/books.png" /></Link>
+                    <Link to={"/email"}><img src="./assets/img/email.png" /></Link>
+                    <Link to={"/keep"}><img src="./assets/img/keep.png" /></Link>
+                    <Link to={"/book"}><img src="./assets/img/books.png" /></Link>
                 </div>
             </section>
         )
