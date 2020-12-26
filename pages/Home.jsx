abcc4731ad6a9ae7db9ff4331df80b6635271193
iShulamit <66863006+iShulamit@users.noreply.github.com>
2020-12-26 15:10:56 +0200

    css home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -10,7 +10,7 @@ export class Home extends React.Component {
         return (
             <section className="home-page">
                 <h1>This is my Appsus</h1>
-                <div className="home-limks">
+                <div className="home-links">
                     <Link to={"/email"}><img src="../assets/img/email.png" /></Link>
                     <Link to={"/keep"}><img src="../assets/img/keep.png" /></Link>
                     <Link to={"/book"}><img src="../assets/img/books.png" /></Link>
